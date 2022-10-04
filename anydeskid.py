801475fdc0bd4eb2b5441e9fd462cd876fcb07c0
Sipo <78769530+SipoXD@users.noreply.github.com>
2022-10-03 22:04:38 -0600

    upload

---
+++ anydeskid.py
@@ -0,0 +1,21 @@
+from asyncio import windows_events, windows_utils
+import shutil
+import time
+import os
+
+windows_user = os.getenv("username")
+
+os.system("@title AnyDesk ID changer made by Sipo#4285")
+
+print("If you encounter any issues with this, please report them on Cheating Community's discord server!")
+print("Initializing in 3 seconds...")
+time.sleep(3)
+os.system("cls")
+
+print("Resetting ID...")
+
+try:
+    shutil.rmtree(r"C:\\Users\\" + windows_user + r"\\AppData\\Roaming\\AnyDesk")
+    input("Success, press any enter to exit...")
+except:
+    input("Failed, press any enter to exit...")
